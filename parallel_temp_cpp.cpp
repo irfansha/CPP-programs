6b55a195c3d43a8978b5615aa3f189d674f113fc
irfansha <irfansha536@gmail.com>
2018-04-25 15:09:12 +0100

    New file added and changes to cpp-code

--- parallel_temp_cpp.cpp
+++ parallel_temp_cpp.cpp
@@ -1,14 +1,12 @@
-/* For now we are assuming we can get all the data into our array.
-   Later we should change it to accomodate large files
-
-*/
 
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <sys/time.h>
 #include <fstream>
 #include <string>
 #include <sys/stat.h>
-#include <thread>
-#include <vector>
+
 
 using namespace std;
 
@@ -17,16 +15,15 @@ string path = "/home/irfan536/programs/N-queens/refining_n_queens/";
 const int n = 2;
 
 const int m = 3;
-int read_file(ifstream &myfile, string in_array[]);
+
+using namespace std;
 
 void process(string in_array[], int len_array, int target_char);
 
-int diagonal_check(string str, int target_char);
-
 void write_file(string out_path, string out_array[], int count);
 
+int diagonal_check( string str, int target_char);
 
-//reading a file 1000 lines at a time and processing it using process function
 
 //next step is to add threads for parallel execution
 int read_file(ifstream &myfile, string in_array[]) {
@@ -50,12 +47,13 @@ int read_file(ifstream &myfile, string in_array[]) {
 }
 
 
+
 //processing the given chunk of array and writing it to desired output file.
-void process(string in_array[], int len_array, int target_char) {
+void process(string in_array[], int len_array, int t_id) {
 
-	string out_array[1000];
+	string out_array[6][1000];
 	int count = 0;
-
+	int target_char = t_id +96 ;
 	for(int i = 0 ; i < len_array ; i++) {
 
 		int result = diagonal_check(in_array[i], target_char);
@@ -64,7 +62,7 @@ void process(string in_array[], int len_array, int target_char) {
 		
 		if (result == 1 ) {
 			//cout << " very good" << endl;
-			out_array[count] = char(target_char) + in_array[i];
+			out_array[t_id][count] = char(target_char) + in_array[i];
 			cout<< out_array[count]<<endl;
 			count ++;
 		}
@@ -73,7 +71,7 @@ void process(string in_array[], int len_array, int target_char) {
 
 	string out_path = "/home/irfan536/programs/N-queens/n_queens_cpp_code/" + to_string(n)+"_file/file"+to_string(n)+"^"+to_string(m)+"^" + to_string(target_char-96) + ".txt";
 	//cout<< out_path<< endl;
-	write_file(out_path, out_array, count);
+	write_file(out_path, out_array, count, t_id);
 }
 
 int diagonal_check( string str, int target_char) {
@@ -92,36 +90,38 @@ int diagonal_check( string str, int target_char) {
 
 }
 
-void write_file(string out_path, string out_array[], int count) {
+
+void write_file(string out_path, string out_array, int count, int t_id) {
 	ofstream my_out_file (out_path);
 	//cout<< count;
 	for (int i = 0 ; i < count ; i ++) {
 		// writing to the out file
-		my_out_file<< out_array[i]<<endl;
+		my_out_file<< out_array[t_id][i]<<endl;
 	} 
 	
 }
 
 
-int main () {
+int main() {
+
+	int target_char;
 	std::vector<std::thread> th;
 	ifstream myfile (path + to_string(n)+"_file/file"+to_string(n)+"^"+to_string(m)+".txt");
 	string in_array[1000];
-	int line_count =read_file(myfile,in_array);
-	for (int t_id = 1; t_id < 7; t_id++) {	
-		
-		int target_char = t_id +96 ;
+	int len_array = 10;
 
-		//th.push_back(std::thread(process,in_array,line_count,target_char));
-		//making it a small char alphabet
+	//Launch a group of threads
+	for (int t_id=1; t_id< 7; ++t_id) {	
+			
+		//th.push_back(std::thread(parallel_sum,i,ar));
+		th.push_back(std::thread(process,in_array,len_array,t_id));
+	}
 
-		
-		process(in_array,line_count,target_char);
-		//for (auto &t : th) {
-		//t.join();
-		//}
-
-		}
+	//Join the threads with the main thread
+	for (auto &t : th) {
+		t.join();
+	}
 
 	return 0;
+
 }
